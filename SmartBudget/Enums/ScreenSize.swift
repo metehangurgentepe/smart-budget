03dad062807e382374b3fc0dd562b1a40a94c8bb
Mete <metehangurgentepe@gmail.com>
2024-03-29 15:00:56 +0300

    first commit

--- SmartBudget/Enums/ScreenSize.swift
+++ SmartBudget/Enums/ScreenSize.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import UIKit
+
+struct ScreenSize {
+    static let width = UIScreen.main.bounds.size.width
+    static let height = UIScreen.main.bounds.size.height
+    static let maxLength = max(ScreenSize.width, ScreenSize.height)
+    static let minLength = min(ScreenSize.width, ScreenSize.height)
+}
--- SmartBudget/Features/Home/HomeVC.swift
+++ SmartBudget/Features/Home/HomeVC.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import Charts
 
 class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
     }
-
-
+    
+    private func configure() {
+        configureExpenseView()
+    }
+    
+    private func configureExpenseView() {
+        var expense = ExpenseView(leftBudget: 100, monthBudget: 200)
+        
+        expense.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+            make.height.equalTo(200)
+        }
+    }
 }
 
