0501e747644e5cd8ee84b76b2af4fa82a45aba2a
Mete <metehangurgentepe@gmail.com>
2024-03-29 14:55:11 +0300

    first commit

---
+++ SmartBudget/CustomViews/Views/ExpenseView.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseView.swift
+//  SmartBudget
+//
+//  Created by Metehan Gürgentepe on 29.03.2024.
+//
+
+import UIKit
+
+class ExpenseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SmartBudget/Enums/ScreenSize.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenSize.swift
+//  SmartBudget
+//
+//  Created by Metehan Gürgentepe on 29.03.2024.
+//
+
+import Foundation
